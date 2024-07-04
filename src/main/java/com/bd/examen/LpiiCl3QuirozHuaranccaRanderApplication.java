feab86a9eaed1a32739ee3b006b588d06c238311
Rander 213 <rander213jsjs@gmail.com>
2024-07-04 10:23:03 -0500

    primer commit

---
+++ src/main/java/com/bd/examen/LpiiCl3QuirozHuaranccaRanderApplication.java
@@ -0,0 +1,13 @@
+package com.bd.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiCl3QuirozHuaranccaRanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiCl3QuirozHuaranccaRanderApplication.class, args);
+	}
+
+}
